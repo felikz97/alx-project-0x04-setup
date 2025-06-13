d7d85fd9b7536139def08425d7a6eb8f260b6d50
felikz97 <felikz.kipkemoi@gmail.com>
2025-06-13 19:14:20 +0300

    1 include a ContextAPI for Global sate management

--- alx-project-0x05/context/CountContext.tsx
+++ alx-project-0x05/context/CountContext.tsx
@@ -1,31 +1,35 @@
-// context/CountContext.tsx
-import { createContext, useContext, useState, ReactNode } from "react";
+import { createContext, useContext,  useState, ReactNode } from "react"
 
-interface CountContextType {
-  count: number;
-  increment: () => void;
-  decrement: () => void;
+interface CountContextProps {
+  count: number
+  increment: () => void
+  decrement: () => void
 }
 
-const CountContext = createContext<CountContextType | undefined>(undefined);
+export const CountContext = createContext<CountContextProps | undefined>(undefined)
 
-export const CountProvider = ({ children }: { children: ReactNode }) => {
-  const [count, setCount] = useState(0);
+export const CountProvider = ({ children }: { children: ReactNode}) => {
 
-  const increment = () => setCount((prev) => prev + 1);
-  const decrement = () => setCount((prev) => prev - 1);
+  const [count, setCount] = useState<number>(0)
+
+  const increment = () => setCount((count ) =>count + 1)
+  const decrement = () => setCount((count) => count > 0 ? count - 1 : 0)
 
   return (
     <CountContext.Provider value={{ count, increment, decrement }}>
       {children}
     </CountContext.Provider>
-  );
-};
+  )
+}
+
+
 
 export const useCount = () => {
-  const context = useContext(CountContext);
+  const context = useContext(CountContext)
+
   if (!context) {
-    throw new Error("useCount must be used within a CountProvider");
+    throw new Error("useCount must be within a Count Provider")
   }
-  return context;
-};
+
+  return context
+}
