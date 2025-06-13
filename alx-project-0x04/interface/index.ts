64e1b1988fe99e3cdf6716c6c08e9404a83b9303
felikz97 <felikz.kipkemoi@gmail.com>
2025-06-13 18:44:43 +0300

    0 Adding counter app to platform with useSate hook

---
+++ alx-project-0x04/interface/index.ts
@@ -0,0 +1,31 @@
+// interfaces/index.ts
+
+import { ReactNode } from "react";
+
+// From pages/index.tsx
+export interface PageRouteProps {
+  pageRoute: string;
+}
+
+// From components/common/Button.tsx
+export type ButtonSize = "small" | "medium" | "large";
+export type ButtonShape = "rounded-sm" | "rounded-md" | "rounded-full";
+
+export interface ButtonProps {
+  buttonLabel: string;
+  buttonBackgroundColor?: string;
+  size?: ButtonSize;
+  shape?: ButtonShape;
+  action?: () => void;
+}
+
+// From components/layouts/Layout.tsx
+export interface LayoutProps {
+  children: ReactNode;
+}
+// From components/common/Modal.tsx
+export interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
